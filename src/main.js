00d3a87895fb9f77744a68a9ebc9d8c1763de448
Anton Rychkov <antrychkov@gmail.com>
2020-11-19 15:39:46 +0500

    Добавил рендеринг меню и статистики

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import {createProfileRating} from "./view/profile-rating.js";
+import { createMenuAndStats } from "./view/menu.js";
 
-const render = (container, element) => {
-  container.insertAdjacentHTML(`beforeend`, element);
+const render = (container, element, place) => {
+  container.insertAdjacentHTML(place, element);
 };
 
 const profileHeader = document.querySelector(`.header`);
+const main = document.querySelector(`.main`);
 
-render(profileHeader, createProfileRating());
+render(profileHeader, createProfileRating(), `beforeend`);
+render(main, createMenuAndStats(), `afterbegin`);
