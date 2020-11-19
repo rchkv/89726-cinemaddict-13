579e1369bfb597fb0370a5926701c21de5d95046
Anton Rychkov <antrychkov@gmail.com>
2020-11-19 19:50:31 +0500

    Добавил рендеринг экстра блоков

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import {createMenuAndStats} from "./view/menu.js";
 import {createFilmcard} from "./view/film-card.js";
 import {createFilmsList} from "./view/films-list.js";
 import {createShowMoreButton} from "./view/show-more-button.js";
+import {createFilmsListExtra} from "./view/film-list-extra.js";
 
 const FILM_COUNT = 5;
+const EXTRA_FILM_COUNT = 2;
 
 const render = (container, element, place) => {
   container.insertAdjacentHTML(place, element);
@@ -17,6 +19,7 @@ render(profileHeader, createProfileRating(), `beforeend`);
 render(main, createMenuAndStats(), `afterbegin`);
 render(main, createFilmsList(), `beforeend`);
 
+const filmsSection = main.querySelector(`.films`);
 const filmsList = main.querySelector(`.films-list`);
 const filmListContainer = filmsList.querySelector(`.films-list__container`);
 
@@ -25,3 +28,15 @@ for (let i = 0; i < FILM_COUNT; i++) {
 }
 
 render(filmsList, createShowMoreButton(), `beforeend`);
+render(filmsSection, createFilmsListExtra(`Top rated`), `beforeend`);
+render(filmsSection, createFilmsListExtra(`Most commented`), `beforeend`);
+
+const filmsListExtra = filmsSection.querySelectorAll(`.films-list--extra`);
+
+filmsListExtra.forEach((element) => {
+  const filmListExtraContainer = element.querySelector(`.films-list__container`);
+
+  for (let i = 0; i < EXTRA_FILM_COUNT; i++) {
+    render(filmListExtraContainer, createFilmcard(), `beforeend`);
+  }
+});
---
+++ src/view/film-list-extra.js
@@ -0,0 +1,7 @@
+export const createFilmsListExtra = (listTitle) => {
+  return `<section class="films-list films-list--extra">
+      <h2 class="films-list__title">${listTitle}</h2>
+
+      <div class="films-list__container"></div>
+  </section>`;
+};
