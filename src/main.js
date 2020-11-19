1935f4adc0b86870cced14c01a5abf30168e7348
Anton Rychkov <antrychkov@gmail.com>
2020-11-19 16:29:11 +0500

    Добавил рендеринг списка фильмов

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import {createProfileRating} from "./view/profile-rating.js";
-import { createMenuAndStats } from "./view/menu.js";
+import {createMenuAndStats} from "./view/menu.js";
+import {createFilmcard} from "./view/film-card.js";
+import {createFilmsList} from "./view/films-list.js";
+
+const FILM_COUNT = 5;
 
 const render = (container, element, place) => {
   container.insertAdjacentHTML(place, element);
@@ -10,3 +14,10 @@ const main = document.querySelector(`.main`);
 
 render(profileHeader, createProfileRating(), `beforeend`);
 render(main, createMenuAndStats(), `afterbegin`);
+render(main, createFilmsList(), `beforeend`);
+
+const filmsList = main.querySelector(`.films-list .films-list__container`);
+
+for (let i = 0; i < FILM_COUNT; i++) {
+  render(filmsList, createFilmcard(), `beforeend`);
+}
