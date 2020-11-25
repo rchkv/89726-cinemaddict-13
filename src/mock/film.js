de4193efe58ccc6fc709c597b9c3b78d75154189
Anton Rychkov <antrychkov@gmail.com>
2020-11-25 17:06:21 +0500

    Продолжаю пилить тестовые данные для фильма

--- src/mock/film.js
+++ src/mock/film.js
@@ -1,4 +1,4 @@
-const path = require('path');
+import path from "path";
 
 const getRandomInt = (a = 0, b = 1) => {
   const lower = Math.ceil(Math.min(a, b));
@@ -12,12 +12,36 @@ const generateTitle = () => {
     `Sagebrush Trail`,
     `The Man with the Golden Arm`,
     `Santa Claus Conquers the Martians`,
-    `Popeye the Sailor Meets Sindbad the Sailor`
+    `Popeye the Sailor Meets Sindbad the Sailor`,
   ];
 
   const randomIndex = getRandomInt(0, titles.length - 1);
 
   return titles[randomIndex];
+};
+
+const generateGenre = () => {
+  const genres = [
+    `Musical`,
+    `Western`,
+    `Drama`,
+    `Comedy`,
+    `Cartoon`,
+  ];
+
+  const randomIndex = getRandomInt(0, genres.length - 1);
+
+  return genres[randomIndex];
+};
+
+const generateDescription = () => {
+  const originalText = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet varius magna, non porta ligula feugiat eget. Fusce tristique felis at fermentum pharetra. Aliquam id orci ut lectus varius viverra. Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum. Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui. Sed sed nisi sed augue convallis suscipit in sed felis. Aliquam erat volutpat. Nunc fermentum tortor ac porta dapibus. In rutrum ac purus sit amet tempus.`;
+  const textSuggestions = originalText
+    .split(`.`)
+    .sort(() => Math.random() - 0.5) // перемешиваем массив
+    .slice(0, getRandomInt(1, 5));
+
+  return textSuggestions.join(`.`);
 };
 
 const generatePosterPath = () => {
@@ -27,7 +51,7 @@ const generatePosterPath = () => {
     `sagebrush-trail.jpg`,
     `santa-claus-conquers-the-martians.jpg`,
     `the-great-flamarion.jpg`,
-    `the-man-with-the-golden-arm.jpg`
+    `the-man-with-the-golden-arm.jpg`,
   ];
 
   const randomIndex = getRandomInt(0, posters.length - 1);
@@ -38,6 +62,10 @@ const generatePosterPath = () => {
 export const generateFilm = () => {
   return {
     title: generateTitle(),
-    poster: generatePosterPath()
+    poster: generatePosterPath(),
+    description: generateDescription(),
+    rating: 9.3,
+    year: getRandomInt(1950, 2020),
+    genre: generateGenre()
   };
 };
--- src/view/film-card.js
+++ src/view/film-card.js
@@ -1,16 +1,16 @@
 export const createFilmcard = (film) => {
-  const {title, poster} = film;
+  const {title, poster, description, rating, year, genre} = film;
 
   return `<article class="film-card">
     <h3 class="film-card__title">${title}</h3>
-    <p class="film-card__rating">8.3</p>
+    <p class="film-card__rating">${rating}</p>
     <p class="film-card__info">
-      <span class="film-card__year">1929</span>
+      <span class="film-card__year">${year}</span>
       <span class="film-card__duration">1h 55m</span>
-      <span class="film-card__genre">Musical</span>
+      <span class="film-card__genre">${genre}</span>
     </p>
     <img src="${poster}" alt="" class="film-card__poster">
-    <p class="film-card__description">Burlesque comic Ralph "Skid" Johnson (Skelly), and specialty dancer Bonny Lee King (Carroll), end up together on a cold, rainy night at a tr…</p>
+    <p class="film-card__description">${description}</p>
     <a class="film-card__comments">5 comments</a>
     <div class="film-card__controls">
       <button class="film-card__controls-item button film-card__controls-item--add-to-watchlist" type="button">Add to watchlist</button>
